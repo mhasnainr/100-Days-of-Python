9082d5776e97cb819d968e584ff566f5c7349838
Hasnain <mhasnainr.415@gmail.com>
2025-02-06 17:50:22 +0500

    Day 1.1: Printing & Debugging

---
+++ Beginner/day-01/index.py
@@ -0,0 +1,87 @@
+# print('Welcome to Python World')
+# print(17)
+# print()
+
+# n1 = int(input())
+# n2 = int(input())
+# print(n1 + n2)
+
+
+# --------------------------- 08: Printing
+
+
+# Printing Modifiers
+
+# print('Lorem Ipsum is simply 'dummy' text of the printing and typesetting industry.')
+# this will provide an error. b/c it's confusing for the system to recognise which is code and which is not
+
+# so use different types of quotation: single in main, and double for  a specific word, and vice versa
+
+
+# 1. print('Lorem Ipsum is simply "dummy" text of the printing and typesetting industry.')
+
+
+# 2. print('Lorem Ipsum is simply \"dummy\" text of the printing and typesetting industry.')
+
+
+# --------------------------- 09: String Manipulation & Code Intelligence
+
+# strategies for writing same code for multiple times [not loop one]
+
+# 1.
+# print('Welcome to Python World')
+# print('Welcome to Python World')
+
+# 2.
+# print('Welcome to Python World\nWelcome to Python World')
+
+
+# combining strings
+
+# print('Welcome' + 'Python')
+
+# 1.
+# print('Welcome' + ' ' + 'Python')
+
+# 2.
+# print('Welcome ' + 'Python')
+
+# 3.
+# print('Welcome' + ' Python')
+
+
+# String Concatenation
+# note: space matters in Python
+# Indentation/ Syntax error in next line, b/c of giving a space
+
+#  print('Welcome' + ' ' + 'Python')
+
+
+# --------------------------- 10: Debugging
+
+# fix code
+
+# 1.
+# print(Welcome to Python World')
+
+# print('Welcome to Python World')     
+
+# 2.
+# print('Welcome to '+' Python World')
+
+# print("Welcome to '+' Python World")
+
+# 3.
+    # print('e.g. print("Welcome" + "World")')
+
+# print('e.g. print("Welcome" + "World")')
+
+# 4.
+# print(('Welcome to Python World')
+
+# print('Welcome to Python World')
+
+
+
+# --------------------------- 11: Input function
+
