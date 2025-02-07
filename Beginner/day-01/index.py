3420bb7efe6d5987fa619e70530f2b2c790d10a7
Hasnain <mhasnainr.415@gmail.com>
2025-02-07 14:07:44 +0500

    Day 1.2: Input function & Variables

--- Beginner/day-01/index.py
+++ Beginner/day-01/index.py
@@ -1,11 +1,11 @@
+# Topics:
+
+# Printing, String Manipulation & Code Intelligence, Debugging, Input function, Variables, Quiz, Project
+
+
 # print('Welcome to Python World')
 # print(17)
 # print()
-
-# n1 = int(input())
-# n2 = int(input())
-# print(n1 + n2)
-
 
 # --------------------------- 08: Printing
 
@@ -64,7 +64,7 @@
 # 1.
 # print(Welcome to Python World')
 
-# print('Welcome to Python World')     
+# print('Welcome to Python World')
 
 # 2.
 # print('Welcome to '+' Python World')
@@ -72,7 +72,7 @@
 # print("Welcome to '+' Python World")
 
 # 3.
-    # print('e.g. print("Welcome" + "World")')
+# print('e.g. print("Welcome" + "World")')
 
 # print('e.g. print("Welcome" + "World")')
 
@@ -82,6 +82,91 @@
 # print('Welcome to Python World')
 
 
-
 # --------------------------- 11: Input function
 
+# print('Enter your name: ')
+# input('Enter your name: ')
+
+# print('Welcome ' + input('Enter your name: '))
+
+
+# Commenting
+
+
+# --------------------------- 12: Coding Ex
+
+# print(input())
+# print(input())
+# print(input())
+
+
+# n1 = int(input())
+# n2 = int(input())
+# print(n1 + n2)
+
+
+# len() function
+
+# numOfLetters = len('Hasnain')
+# print(numOfLetters)
+
+
+# find num of letters of input written:
+
+# print(len(input('Enter your name: ')))
+
+
+# --------------------------- 13: Variables
+
+# name = 'Ali'
+# print(name)
+
+# name = 'Abid'
+# print(name)
+
+
+# input('Enter your name: ')
+# name = input('Enter your name: ')
+# print('Name of coder is ' + name)
+# print("Coder's name is: " + name)
+
+
+# name = input('Enter your name: ')
+# length = len(name)
+# print(length)
+
+
+# --------------------------- 14: Coding Ex [Variables]
+
+# Interchange variables
+
+# a = input()
+# b = input()
+
+# c = a
+# a = b
+# b = c
+
+# print("a: " + a)
+# print("b: " + b)
+
+
+
+# --------------------------- 15: Variable Naming
+
+# note: varialbe naming should be readable
+# can't start with numbers, characters, etc
+
+
+
+# --------------------------- 16: Quiz [httml file]
+
+
+# --------------------------- 17: Project: Brand Name generator
+
+# print('Welcome to Band Name Generator Project')
+
+# city_name = input("What's the name of your city you grew up?\n")
+# pet_name = input("What's your pet name?\n")
+
+# print("Your band name could be " + city_name + ' ' + pet_name)
