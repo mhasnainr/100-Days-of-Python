633d92cac58dc52933223f9680facab31d304bcf
Hasnain <mhasnainr.415@gmail.com>
2025-02-07 14:09:50 +0500

    Day 1.2: slight changes

--- Beginner/day-01/index.py
+++ Beginner/day-01/index.py
@@ -160,13 +160,3 @@
 
 
 # --------------------------- 16: Quiz [httml file]
-
-
-# --------------------------- 17: Project: Brand Name generator
-
-# print('Welcome to Band Name Generator Project')
-
-# city_name = input("What's the name of your city you grew up?\n")
-# pet_name = input("What's your pet name?\n")
-
-# print("Your band name could be " + city_name + ' ' + pet_name)
