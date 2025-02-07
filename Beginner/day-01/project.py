d0625c35f558c389be1d5bce8331848746a83e42
Hasnain <mhasnainr.415@gmail.com>
2025-02-07 14:12:29 +0500

    Day 1.2- Project: Brand Name Generator

---
+++ Beginner/day-01/project.py
@@ -0,0 +1,8 @@
+# --------------------------- 17: Project: Brand Name generator
+
+print('Welcome to Band Name Generator Project')
+
+city_name = input("What's the name of your city you grew up?\n")
+pet_name = input("What's your pet name?\n")
+
+print("Your band name could be " + city_name + ' ' + pet_name)
