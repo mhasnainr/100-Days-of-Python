6f7884e7ab4877e3c445f007b9e72910bf5213c6
Hasnain <mhasnainr.415@gmail.com>
2025-02-17 15:01:54 +0500

    Day 6.2: Functions & While loop

--- Beginner/day-06/index.py
+++ Beginner/day-06/index.py
@@ -1,3 +1,78 @@
 # Topics:
 
 # Functions & While loop
+
+# ------------------- 02: Defining and Calling Function
+
+# print('Hello')
+# num_char = len('Hello')
+# print(num_char)
+
+#
+
+# Defining a function
+
+# def my_function():
+#     print('Hello')
+
+
+# calling function
+# my_function()
+
+#
+
+# ------------------- 03:
+
+#
+
+# ------------------- 04: Indentation [ Spaces vs. Tabs ]
+
+# Using 'Space' is preferred
+
+#
+
+# ------------------- 05: Quiz [Indentation]
+
+#
+
+# ------------------- 06: While Loop
+
+# --- Recap of for loop
+# 1- using list of --
+
+# for item in list_of_items:
+# do something for each item
+
+
+# 2- using range(a, b)
+# for num in range(a,b):
+# print(num)
+
+#
+
+# --- While: loop continues until the condition becomes false
+
+# while something_is_true:
+# do something repeatedly
+
+# def jump():
+#     print('Jump')
+
+
+# jump()
+
+# num_of_hurdles = 6
+# while num_of_hurdles > 0:
+#     jump()
+#     num_of_hurdles -= 1
+#     print(num_of_hurdles)
+
+#
+
+# ------------------- 07: Hurdles Challenge
+
+#
+
+# ------------------- 08: Jumping over hurdles with variable heights
+
+#
